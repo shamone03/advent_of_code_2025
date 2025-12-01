4dd7c8e0508b9b8ca3646b0e39ad5b59619cca16
shamone <aryahjkannan@gmail.com>
2025-12-01 19:12:30 -0400

    part 1

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,78 @@
+use crate::day1::Rotation;
+
 mod day1 {
     fn get_input() -> &'static str {
         include_str!("../input/day1.txt")
+        //         "L68
+        // L30
+        // R48
+        // L5
+        // R60
+        // L55
+        // L1
+        // L99
+        // R14
+        // L82"
     }
 
-    #[derive(Clone, Copy)]
-    struct Rotation(u32);
+    #[derive(Clone, Copy, Debug)]
+    pub struct Rotation(pub u32);
+
+    #[derive(Clone, Copy, Debug)]
+    pub enum Direction {
+        Left(u32),
+        Right(u32),
+    }
 
     impl Rotation {
-        fn left(mut self, n: u32) -> Self {
-            self.0 -= n;
+        pub fn left(mut self, n: u32) -> Self {
+            if n > self.0 {
+                self.0 = (100 - ((n - self.0) % 100)) % 100;
+            } else {
+                self.0 -= n;
+                self.0 %= 100;
+            }
             self
         }
-        fn right(mut self, n: u32) -> Self {
+
+        pub fn right(mut self, n: u32) -> Self {
             self.0 += n;
+            self.0 %= 100;
             self
+        }
+
+        pub fn rotate(self, direction: Direction) -> Self {
+            let ret = match direction {
+                Direction::Left(amount) => self.left(amount),
+                Direction::Right(amount) => self.right(amount),
+            };
+            assert!(self.0 <= 99);
+            ret
         }
     }
 
     pub fn part1() -> u32 {
-        let rotate = Rotation(50);
+        let mut rotate = Rotation(50);
         get_input()
             .lines()
-            .filter_map(|line| match line.chars().next()? {
-                'L' => Some(
-                    rotate.left(
-                        line.chars()
-                            .skip(1)
-                            .collect::<String>()
-                            .parse::<u32>()
-                            .ok()?,
-                    ),
-                ),
-                'R' => Some(
-                    rotate.right(
-                        line.chars()
-                            .skip(1)
-                            .collect::<String>()
-                            .parse::<u32>()
-                            .ok()?,
-                    ),
-                ),
-                _ => None,
+            .map(|line| {
+                let amount = line
+                    .chars()
+                    .skip(1)
+                    .collect::<String>()
+                    .parse::<u32>()
+                    .expect("Invalid input");
+
+                match line.chars().next().expect("Invalid input") {
+                    'L' => Direction::Left(amount),
+                    'R' => Direction::Right(amount),
+                    _ => panic!("Invalid input"),
+                }
+            })
+            .map(|direction| {
+                dbg!(rotate, direction);
+                rotate = rotate.rotate(direction);
+                rotate
             })
             .filter(|rotation| rotation.0 == 0)
             .count()
@@ -50,5 +82,6 @@ mod day1 {
 }
 
 fn main() {
-    println!("day 1 part 1: {}", day1::part1());
+    println!("{:?}", day1::part1());
+    println!("{:?}", Rotation(52).left(552))
 }
